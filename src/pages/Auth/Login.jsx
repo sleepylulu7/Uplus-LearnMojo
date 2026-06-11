f53561d0e4b039f679de0f6cbad0cce2c4188db5
msisouphanh <marcus.sisouphanh@gmail.com>
2026-06-11 13:36:43 -0400

    Fix routing: reintroduce previously AppRouter line and add missing login route

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -3,8 +3,6 @@ import googleSvg from "../../assets/icons/google.svg";
 import facebookSvg from "../../assets/icons/facebook_blue.svg";
 import "./auth.css";
 
-const hideFooter = location.pathname === "/signin";
-
 function Login({ copy }) {
   return (
     <main id="auth" className="auth-page">
@@ -12,35 +10,35 @@ function Login({ copy }) {
         <img id="mascot" src={mascotSvg} alt="" aria-hidden="true" />
       </div>
       <div className="auth-heading">
-        <h1>{copy.titles.login}</h1>
+        <h1>{copy.auth.titles.login}</h1>
       </div>
 
       <div className="auth-form-content">
         <form className="auth-form">
-          <label for="email">{copy.fields.emailOrUsername}</label>
-          <input id="email" placeholder={copy.placeholder.email} />
+          <label for="email">{copy.auth.fields.emailOrUsername}</label>
+          <input id="email" placeholder={copy.auth.placeholder.email} />
 
-          <label for="password">{copy.fields.password}</label>
+          <label for="password">{copy.auth.fields.password}</label>
           <input id="password" type="password" />
 
-          <a>{copy.actions.forgotPassword}</a>
+          <a>{copy.auth.actions.forgotPassword}</a>
 
           <button className="signin-button" type="submit">
-            {copy.actions.signin}
+            {copy.auth.actions.signin}
           </button>
         </form>
       </div>
 
       <div className="separator-content">
         <hr />
-        <p>{copy.separators.social}</p>
+        <p>{copy.auth.separators.social}</p>
         <hr />
       </div>
 
       <div className="social-signup">
         <button type="button" className="social-btn">
           <img src={googleSvg} alt="google icon" className="google-icon" />
-          {copy.socialAuth.google}
+          {copy.auth.socialAuth.google}
         </button>
         <button type="button" className="social-btn">
           <img
@@ -48,7 +46,7 @@ function Login({ copy }) {
             alt="facebook icon"
             className="facebook-icon"
           />
-          {copy.socialAuth.facebook}
+          {copy.auth.socialAuth.facebook}
         </button>
       </div>
     </main>
